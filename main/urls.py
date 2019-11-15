0ab45417803fca9d102fd9f79ce3b8e76671b02d
zyx124 <zhaoyuxin124@gmail.com>
2019-11-14 22:32:11 -0500

    modify urls

--- main/urls.py
+++ main/urls.py
@@ -6,8 +6,8 @@ app_name = 'main'  # namespacing of urls
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
-    path('register/', views.register, name='register'),
-    path('logout/', views.logout_request, name='logout'),
-    path('login/', views.login_request, name='login'),
+    path('register', views.register, name='register'),
+    path('logout', views.logout_request, name='logout'),
+    path('login', views.login_request, name='login'),
     path('<single_slug>', views.single_slug, name="single_slug")
 ]
