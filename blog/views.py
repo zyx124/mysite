8cacff19888cb81e907195f9e16139669a67b35e
zyx124 <zhaoyuxin124@gmail.com>
2019-11-29 22:26:51 -0500

    add html5 to the page view

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+from .models import Post
 
 
 def index(request):
-    return HttpResponse("Welcome")
+    post_list = Post.objects.all().order_by('-created_time')
+    return render(request, 'blog/index.html', context={
+        'post_list': post_list
+    })
 
